5e08e684d567d22efb2053e6ae899e70a33ec953
Edith <edithmar996@gmail.com>
2026-07-02 22:19:02 -0300

    Arquivo novo de estudos

---
+++ aulafuncao.py
@@ -0,0 +1,20 @@
+#Exemplo: Calculas imposto de produtos utilizando uma função
+#Não precisaria repetir o mesmo cálculo toda vez
+#Criar uma função calcular_imposto() e usá-la quando precisar
+
+
+
+# def saudacao(nome):
+    #print(f"OLá! {nome}! Bem-vindo ao curso de Python.")
+
+#saudacao("Maria")
+
+#RETORNO DE VALORES
+
+def somar(a, b):
+    return a + b
+
+#Chamando a função e armazenando o resultado
+resultado = somar(5, 3)
+print(f"A soma é {resultado}")
+
