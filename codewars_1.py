45fe4532014855eed1a4985f63a2bac85bca6c1d
Edith <edithmar996@gmail.com>
2026-07-16 13:20:06 -0300

    Resolvendo codewars pela primeira vez

---
+++ codewars_1.py
@@ -0,0 +1,8 @@
+def solution(string):
+    return string[::-1]
+
+if __name__ == "__main__":
+    
+    print("Reverse of world: ", solution("world"))
+    print("Reverse of word: ", solution("word"))
+
